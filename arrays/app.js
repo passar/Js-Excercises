ee71e2642a215c7699de95d67ef8b22884345c59
Andreas Sandskär <ekrarna@gmail.com>
2019-02-15 15:53:35 +0100

    skissat på svar

--- arrays/app.js
+++ arrays/app.js
@@ -5,11 +5,19 @@ const shoppingList = ["Tomatoes", "Eggs", "Potatoes", "Ham", "Milk"];
 
 /* --- And here --- */
 
-// Loop through the array to print each item on new line
+let body = document.getElementsByTagName('body');
 
+// Loop through the array to print each item on new line
+for(let loop=0;loop>shoppingList.length();loop++){
+  //skriva ut till dokumentet
+  shoppingList[loop]
+  // preventDefault();
+  
+}
 
 // if the number of items is more than 3 alert('Man, how can I carry so much?');
 
 
 // Remove the last item, Milk, and console.log it to the console
 
+console.log(shoppingList.pop())
